db0067bbfcaa44f38ed6aa34760dcffad8cab8c4
mihaiprodann <florin_mihai.prodan@stud.fils.upb.ro>
2024-10-19 11:55:03 +0300

    set up environment

---
+++ main.py
@@ -0,0 +1,18 @@
+import discord
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+token = os.getenv("TOKEN")
+
+class Client(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+
+client = Client(intents=intents)
+client.run(token)
